f6f3ddc9a9fa0cf6f8884382504c9601a551fe12
Roshan Sanjeev <roshan.sanjeev@gmail.com>
2024-12-23 23:48:32 -0800

    function for sending a video into media pipe(under contstruction) and function for sending live webcam data into mediapipe and animating it done

--- Training/main.py
+++ Training/main.py
@@ -1,28 +1,53 @@
+import cv2
+import mediapipe as mp
 
+#initialize MediaPipe Pose model for detecting human body landmarks(like joints)
+mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
 
-# Open webcam
-cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+def readWebCam():
+    #Open webcam(cap is open an open cv video object) -or- open video file
+    cap = cv2.VideoCapture(0)
+    #as long as webcam is open
+    while cap.isOpened():
+        #cap.read captures a single frame from the webcam
+        #success is boolean tracking whether frame was read successfully
+        #frame is the captured frame
+        success, frame = cap.read()
+        if not success:
+            break
 
-    # Convert frame to RGB
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Convert OpenCV's BGR to RGB format
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Process frame
-    result = pose.process(frame_rgb)
+        # Analyzes the RGB frame to detect human body landmarks
+        #result stores the landmarks(joints like the shoulders or knees)
+        result = pose.process(frame_rgb)
 
-    # Draw landmarks
-    mp_drawing = mp.solutions.drawing_utils
-    mp_drawing.draw_landmarks(frame, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+        # Draw landmarks
+        mp_drawing = mp.solutions.drawing_utils
+        mp_drawing.draw_landmarks(frame, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
-    # Show frame
-    cv2.imshow('MediaPipe Pose', frame)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
+        # Show frame
+        cv2.imshow('MediaPipe Pose', frame)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+        
+    cap.release()
+    cv2.destroyAllWindows()
 
-cap.release()
-cv2.destroyAllWindows()
+def readDownload():
+    cap = cv2.VideoCapture('Videos/FrontSquat.mp4')
+    
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            break
+        
+        cv2.imshow("Frame", frame)
+        cv2.waitkey(1)
+        
 
+readWebCam()
+#-or-
+#readDownload()
