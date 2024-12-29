c6c9539bc1a852727357de593082bbea2225e62d
Roshan Sanjeev <roshan.sanjeev@gmail.com>
2024-12-29 00:25:22 -0800

    progress with uploaded detection, and more detailed code

--- Training/main.py
+++ Training/main.py
@@ -1,9 +1,15 @@
 import cv2
 import mediapipe as mp
-
+import time
 #initialize MediaPipe Pose model for detecting human body landmarks(like joints)
 mp_pose = mp.solutions.pose
-pose = mp_pose.Pose()
+pose = mp.solutions.pose.Pose(
+    static_image_mode =True,
+    model_complexity = 1,
+    smooth_landmarks = True,
+    min_detection_confidence = 0.5,
+    min_tracking_confidence = 0.5
+)
 
 def readWebCam():
     #Open webcam(cap is open an open cv video object) -or- open video file
@@ -37,14 +43,28 @@ def readWebCam():
     cv2.destroyAllWindows()
 
 def readDownload():
-    cap = cv2.VideoCapture('/Users/roshansanjeev/Desktop/AdvancedComputerVision/Videos/SideAirSquat.mp4')
-    
+    cap = cv2.VideoCapture('/Users/roshansanjeev/Desktop/AdvancedComputerVision/Videos/FrontSquat.mp4')
+    pTime = 0
+    VidSpeed = time.time()
     while cap.isOpened():
         success, frame = cap.read()
         if not success:
             break
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        result = pose.process(frame_rgb)
         
-        # Show frame
+        if result.pose_landmarks:
+            mp_drawing = mp.solutions.drawing_utils
+            mp_drawing.draw_landmarks(frame, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+            
+        print(result.pose_landmarks)
+        #show framerate
+        cTime = time.time()
+        fps = 1/(cTime-pTime)
+        pTime = cTime
+        cv2.putText(frame, str(int(fps)), (70,50), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,0), 3)
+        
+        #play video frame 
         cv2.imshow('MediaPipe Pose', frame)
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
